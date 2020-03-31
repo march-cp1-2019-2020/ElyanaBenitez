06a649750dc4414ef02b4e40346230e9665e5765
E-12 <58489582+E-12@users.noreply.github.com>
2020-03-31 13:04:11 -0500

    Add files via upload

---
+++ Exercises/Week 12 Homework Assignment/src/murch/HelloWorld.java
@@ -0,0 +1,15 @@
+package murch;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+		
+		LocalDateTime localDate = LocalDateTime.now();
+		System.out.println(localDate);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-YY h:mm a");
+		System.out.println(dtf.format(localDate));
+	}
+}
